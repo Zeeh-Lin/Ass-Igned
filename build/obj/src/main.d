396e1542a0a99f983d803382b2b6af4a795bc771
Lin Zihan <ZihanLin501@163.com>
2025-11-29 14:32:19 +0800

    chore(git): stop tracking build directory

--- build/obj/src/main.d
+++
@@ -1,3 +0,0 @@
-/home/zeehlin/Ass-Igned/build/obj/src/main.o: src/main.c \
- /home/zeehlin/Ass-Igned/include/common.h \
- /home/zeehlin/Ass-Igned/include/log.h
--- build/obj/src/utils/log.d
+++
@@ -1,2 +0,0 @@
-/home/zeehlin/Ass-Igned/build/obj/src/utils/log.o: src/utils/log.c \
- /home/zeehlin/Ass-Igned/include/log.h
